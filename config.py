8a202265da0ad5a5c549d3654ea025073cb4dd58
Ilya Grobovskikh <hurfydev@gmail.com>
2023-10-08 23:14:55 +0300

    Reworked config.py

--- config.py
+++ config.py
@@ -1,5 +1,45 @@
 from decouple import config
 
-GITHUB_TOKEN = config('GITHUB_TOKEN')
-GITHUB_REPO  = config('GITHUB_REPO')
-OPENAI_TOKEN = config('OPENAI_TOKEN')
+
+def github_token():
+    """
+    GitHub Token Validation
+    :return: str: GitHub token
+    """
+    token = config('GITHUB_TOKEN')
+
+    if token.startswith('<') or not token:
+        raise Exception('Invalid or missing GITHUB_TOKEN in the .env file')
+
+    return token
+
+
+def github_repo():
+    """
+    GitHub Repository Validation
+    :return: str: GitHub repository
+    """
+    repo = config('GITHUB_REPO')
+
+    if repo.startswith('<')  or not repo:
+        raise Exception('Invalid or missing GITHUB_REPO in the .env file')
+
+    return repo
+
+
+def openai_token():
+    """
+    OpenAI Token Validation
+    :return: str: OpenAI token
+    """
+    token = config('OPENAI_TOKEN')
+
+    if token.startswith('<') or not token:
+        raise Exception('Invalid or missing OPENAI_TOKEN in the .env file')
+
+    return token
+
+
+GITHUB_TOKEN = github_token()
+GITHUB_REPO  = github_repo()
+OPENAI_TOKEN = openai_token()
--- main.py
+++ main.py
@@ -1,35 +1,17 @@
-from config   import GITHUB_TOKEN, GITHUB_REPO, OPENAI_TOKEN
-from github   import github
-from json     import dump, load
-from os       import path, mkdir
-
-
-# Primitive token validation
-def validate_credits():
-    """
-    The function primitively checks the correctness of tokens entered by the user
-    In case of problems will cause an error with a detailed description
-
-    :return: bool: True
-    """
-    if GITHUB_TOKEN.startswith('<') or not GITHUB_TOKEN:
-        raise Exception('Invalid or missing GITHUB_TOKEN in the .env file')
-
-    if GITHUB_REPO.startswith('<')  or not GITHUB_REPO:
-        raise Exception('Invalid or missing GITHUB_REPO in the .env file')
-
-    if OPENAI_TOKEN.startswith('<') or not OPENAI_TOKEN:
-        raise Exception('Invalid or missing OPENAI_TOKEN in the .env file')
-
-    return True
+from config          import GITHUB_TOKEN, GITHUB_REPO, OPENAI_TOKEN
+from github.github   import fetch_issue
+from github.readme   import fetch_readme
+from json            import dump, load
+from os.path         import exists
+from os              import mkdir
 
 
 def main():
-    if not path.exists('storage'):
+    if not exists('storage'):
         mkdir('storage')
 
-    # readme = github.fetch_readme(GITHUB_TOKEN, GITHUB_REPO)
-    # issues = github.fetch_issue(GITHUB_TOKEN, GITHUB_REPO)
+    # readme = fetch_readme()
+    # issues = fetch_issue()
 
     # Write README to .md
     # with open(f'storage/README.md', 'w', encoding='UTF-8', newline='') as file:
@@ -45,5 +27,4 @@ def main():
 
 
 if __name__ == '__main__':
-    if validate_credits():
-        main()
+    main()
