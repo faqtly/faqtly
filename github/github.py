f57380a59aa1260c2ab5860b6fa3d7c3dcddf0ed
Ilya Grobovskikh <hurfydev@gmail.com>
2023-10-08 23:16:36 +0300

    Fetch main info(Description, Total issues count)

--- github/github.py
+++ github/github.py
@@ -1,12 +1,7 @@
 from config    import GITHUB_TOKEN, GITHUB_REPO
-from requests  import get
+from requests  import get, post
 from json      import dump
 from datetime  import datetime
-
-HEADERS = {
-        'Authorization': f'token {GITHUB_TOKEN}',
-        'Accept'       : 'application/vnd.github.v3.raw'
-}
 
 
 # Debug function
@@ -26,6 +21,40 @@ def time_spent(func):
     return wrapper
 
 
+def fetch_repo_info():
+    """
+    This function returns general repository data, description, number of issues
+    * issues count - issues, pull requests, WIP discussions
+    :return: tuple: (Repository description, Repository issues count)
+    """
+    headers = {
+        'Authorization': f'Bearer {GITHUB_TOKEN}'
+    }
+    url         = fr'https://api.github.com/graphql'
+    owner, repo = GITHUB_REPO.split('/')
+    data        = {'query': f'''
+    {{
+      repository(owner: "{owner}", name: "{repo}") {{
+        description
+        issues {{totalCount}}
+        pullRequests(states: [OPEN, CLOSED, MERGED]) {{totalCount}}
+      }}
+    }}
+    '''}
+
+    request = post(url, json=data, headers=headers)
+    response = request.json()['data']['repository']
+
+    description = response['description']
+    issues_count = response['issues']['totalCount'] + response['pullRequests']['totalCount']
+
+    # Convert description to plain text, removes unnecessary characters. Thanks a lot, Inlife! :D
+    if description is not None:
+        description = sub(r'[^a-zA-Zа-яА-Я0-9\s.,!?-]', '', response['description']).strip()
+
+    return description, issues_count
+
+
 def fetch_comments(url: str):
     """
     The function sends a request for comments if the response is not an empty .json file and increments the page index
@@ -33,13 +62,17 @@ def fetch_comments(url: str):
     :param url:   str: GitHub authorization token
     :return:     list: List of issue comments
     """
+    headers    = {
+        'Authorization': f'token {GITHUB_TOKEN}',
+        'Accept': 'application/vnd.github.v3.raw'
+    }
     page_index = 1  # Let's start with one, because page 1 is equivalent to page 0
     comments   = []
 
     while True:
         params   = {'page': page_index, 'per_page': 100}
-        requests = get(url, params=params, headers=HEADERS)
-        response = requests.json()
+        request  = get(url, params=params, headers=headers)
+        response = request.json()
 
         if response:
             for comment in response:
@@ -60,13 +93,17 @@ def fetch_issue():
     the page index by 1 each iteration. If .json is empty, the loop will break
     :return:     list: List of issues
     """
+    headers    = {
+        'Authorization': f'token {GITHUB_TOKEN}',
+        'Accept': 'application/vnd.github.v3.raw'
+    }
     page_index = 1  # Let's start with one, because page 1 is equivalent to page 0
     issues     = []
 
     while True:
         url      = fr'https://api.github.com/repos/{GITHUB_REPO}/issues'
         params   = {"state": "all", "page": page_index, "per_page": 100}
-        request  = get(url, params=params, headers=HEADERS)
+        request  = get(url, params=params, headers=headers)
         response = request.json()
         print(f'Fetching: {page_index}')
 
